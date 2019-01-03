784872082a2fb790137fce24272d1064c1319470
Mina Shehata <minashehatacs@gmail.com>
2019-01-03 19:06:34 +0200

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Mina Shehata on 1/3/19.
+//  Copyright © 2019 Mina Shehata. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
